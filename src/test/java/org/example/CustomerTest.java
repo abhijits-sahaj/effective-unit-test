18e7cb0ea7cd45cdd7ff32cb010b9026053ca12b
abhijits <abhijits@sahaj.ai>
2025-07-17 17:23:02 +0530

    Moves the setup code for the test case closer to the test case, still uses Object Mother

--- src/test/java/org/example/CustomerTest.java
+++ src/test/java/org/example/CustomerTest.java
@@ -9,48 +9,49 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class CustomerTest {
 
   Customer john, steve, pat, david;
-  String johnName = "John",
-          steveName = "Steve",
-          patName = "Pat",
-          davidName = "David";
   Customer[] customers;
 
   @BeforeEach
   public void setup() {
-    david = ObjectMother.customerWithNoRentals(davidName);
-    john = ObjectMother.customerWithOneNewRelease(johnName);
-    pat = ObjectMother.customerWithOneOfEachRentalType(patName);
-    steve = ObjectMother.customerWithOneNewReleaseAndOneRegular(steveName);
+    david = ObjectMother.customerWithNoRentals("David");
+    john = ObjectMother.customerWithOneNewRelease("John");
+    pat = ObjectMother.customerWithOneOfEachRentalType("Pat");
+    steve = ObjectMother.customerWithOneNewReleaseAndOneRegular("Steve");
     customers = new Customer[]{david, john, steve, pat};
   }
 
   @Test
   public void getName() {
-    assertEquals(davidName, david.getName());
-    assertEquals(johnName, john.getName());
-    assertEquals(steveName, steve.getName());
-    assertEquals(patName, pat.getName());
+    assertEquals("David", david.getName());
+    assertEquals("John", john.getName());
+    assertEquals("Steve", steve.getName());
+    assertEquals("Pat", pat.getName());
   }
 
   @Test
   void shouldReturnRentalStatementOfACustomerGivenTheyHaventRentedAnythingYet() {
+    Customer noRentalCustomerDavid = ObjectMother.customerWithNoRentals("David");
+
     assertEquals("Rental record for David\n" +
             "Amount owed is 0.0\n" +
-            "You earned 0 frequent renter points", david.statement()
+            "You earned 0 frequent renter points", noRentalCustomerDavid.statement()
     );
   }
 
   @Test
-  public void johnStatement() {
+  void shouldReturnRentalStatementOfACustomerGivenTheyHaveRentedNewlyReleaseMovie() {
+    Customer newReleaseCustomerJohn = ObjectMother.customerWithOneNewRelease("John");
+
     assertEquals("Rental record for John\n" +
-                    "\tGodfather 4 9.0\n" +
-                    "Amount owed is 9.0\n" +
-                    "You earned 2 frequent renter points",
-            john.statement());
+            "\tGodfather 4 9.0\n" +
+            "Amount owed is 9.0\n" +
+            "You earned 2 frequent renter points", newReleaseCustomerJohn.statement());
   }
 
   @Test
   public void shouldReturnRentalStatementOfACustomerGivenTheyHaveRentedOneMovieOfEachType() {
+    Customer eachTypeMovieCustomerPat = ObjectMother.customerWithOneOfEachRentalType("Pat");
+
     assertEquals(
             "Rental record for Pat\n" +
                     "\tGodfather 4 9.0\n" +
@@ -58,54 +59,64 @@ public class CustomerTest {
                     "\tLion King 1.5\n" +
                     "Amount owed is 14.0\n" +
                     "You earned 4 frequent renter points",
-            pat.statement());
+            eachTypeMovieCustomerPat.statement());
   }
 
   @Test
   public void shouldReturnRentalStatementOfACustomerGivenTheyHaveRentedOneNewReleaseOneRegularMovie() {
+    Customer newReleaseAndRegularCustomerSteve = ObjectMother.customerWithOneNewReleaseAndOneRegular("Steve");
+
     assertEquals("Rental record for Steve\n" +
                     "\tGodfather 4 9.0\n" +
                     "\tScarface 3.5\n" +
                     "Amount owed is 12.5\n" +
                     "You earned 3 frequent renter points",
-            steve.statement());
+            newReleaseAndRegularCustomerSteve.statement());
   }
 
   @Test
   public void shouldReturnHTLMLRentalStatementOfACustomerGivenTheyHaventRentedAnythingYet() {
+    Customer newReleaseCustomerDavid = ObjectMother.customerWithOneNewRelease("David");
+
     assertEquals("<h1>Rental record for <em>David</em></h1>\n" +
             "<p>Amount owed is <em>0.0</em></p>\n" +
-            "<p>You earned <em>0 frequent renter points</em></p>", david.htmlStatement());
+            "<p>You earned <em>0 frequent renter points</em></p>", newReleaseCustomerDavid.htmlStatement());
   }
 
   @Test
   public void shouldReturnHTMLRentalStatementOfACustomerGivenTheyHaveRentedOnlyOneMovie() {
+    Customer newReleaseCustomerJohn = ObjectMother.customerWithOneNewRelease("John");
+
     assertEquals("<h1>Rental record for <em>John</em></h1>\n" +
                     "<p>Godfather 4 9.0</p>\n" +
                     "<p>Amount owed is <em>9.0</em></p>\n" +
                     "<p>You earned <em>2 frequent renter points</em></p>",
-            john.htmlStatement());
+            newReleaseCustomerJohn.htmlStatement());
   }
 
   @Test
   public void shouldReturnHTMLRentalStatementOfACustomerGivenTheyHaveRentedOneMovieOfEachType() {
+    Customer eachTypeMovieCustomerPat = ObjectMother.customerWithOneOfEachRentalType("Pat");
+
     assertEquals("<h1>Rental record for <em>Pat</em></h1>\n" +
                     "<p>Godfather 4 9.0</p>\n" +
                     "<p>Scarface 3.5</p>\n" +
                     "<p>Lion King 1.5</p>\n" +
                     "<p>Amount owed is <em>14.0</em></p>\n" +
                     "<p>You earned <em>4 frequent renter points</em></p>",
-            pat.htmlStatement());
+            eachTypeMovieCustomerPat.htmlStatement());
   }
 
   @Test
   public void shouldReturnHTMLRentalStatementOfACustomerGivenTheyHaveRentedOneNewReleaseOneRegularMovie() {
+    Customer newReleaseAndRegularCustomerSteve = ObjectMother.customerWithOneNewReleaseAndOneRegular("Steve");
+
     assertEquals("<h1>Rental record for <em>Steve</em></h1>\n" +
                     "<p>Godfather 4 9.0</p>\n" +
                     "<p>Scarface 3.5</p>\n" +
                     "<p>Amount owed is <em>12.5</em></p>\n" +
                     "<p>You earned <em>3 frequent renter points</em></p>",
-            steve.htmlStatement());
+            newReleaseAndRegularCustomerSteve.htmlStatement());
   }
 
 
