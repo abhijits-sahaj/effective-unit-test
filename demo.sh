1b0aa124dc34ca45f55ce9b0eaed655822173cee
abhijits <abhijits@sahaj.ai>
2025-07-22 12:35:52 +0530

    Adds checkpoint-10 in demo.sh

--- demo.sh
+++ demo.sh
@@ -3,7 +3,7 @@
 
 COMMITS=(
   checkpoint-0-starting-point  # initial setup
-  checkpoint-1-separate-test-case4a8ce61  # added feature A
+  checkpoint-1-separate-test-case  # added feature A
   checkpoint-2-expect-literals  # refactored method
   checkpoint-3-rename-test-cases  # added tests
   checkpoint-4-inline-setup
@@ -12,6 +12,8 @@ COMMITS=(
   checkpoint-7-first-solitary-test
   checkpoint-8-price-solitary-test
   checkpoint-9-move-pricing-to-type
+  checkpoint-10-customer-test-mocks
+  fa3ada0
 )
 
 for commit in "${COMMITS[@]}"
