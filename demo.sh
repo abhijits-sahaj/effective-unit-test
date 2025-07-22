fa3ada0e384069a27fe3d1314960b8b6bbdb0193
abhijits <abhijits@sahaj.ai>
2025-07-22 12:30:54 +0530

    Replaces commit numbers with tag in demo.sh

--- demo.sh
+++ demo.sh
@@ -2,10 +2,16 @@
 # demo.sh
 
 COMMITS=(
-  3d55db8  # initial setup
-  4a8ce61  # added feature A
-  647d006  # refactored method
-  deaab7e  # added tests
+  checkpoint-0-starting-point  # initial setup
+  checkpoint-1-separate-test-case4a8ce61  # added feature A
+  checkpoint-2-expect-literals  # refactored method
+  checkpoint-3-rename-test-cases  # added tests
+  checkpoint-4-inline-setup
+  checkpoint-5-remove-setup
+  checkpoint-6-use-builders
+  checkpoint-7-first-solitary-test
+  checkpoint-8-price-solitary-test
+  checkpoint-9-move-pricing-to-type
 )
 
 for commit in "${COMMITS[@]}"
